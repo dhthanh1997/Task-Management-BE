c95a0b10d9ebcbcdcb7b4ff92253e64c10358dd7
BACHTUVU\PC <bachtuvu.dev@gmail.com>
2022-12-14 02:50:17 -0500

    up

--- src/main/java/com/ansv/taskmanagement/repository/CommentRepository.java
+++ src/main/java/com/ansv/taskmanagement/repository/CommentRepository.java
@@ -3,15 +3,19 @@ package com.ansv.taskmanagement.repository;
 import com.ansv.taskmanagement.model.Comment;
 import com.ansv.taskmanagement.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
 
+    @Transactional
+    @Modifying
     @Query(value = "DELETE FROM comment WHERE id IN :listId", nativeQuery = true)
     Integer deleteByListId(@Param("listId") List<Long> listId);
 
