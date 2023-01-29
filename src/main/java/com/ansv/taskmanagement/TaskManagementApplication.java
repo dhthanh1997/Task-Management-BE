b976414fa88e64e02736ff82ea454605f5dc040d
BACHTUVU\PC <bachtuvu.dev@gmail.com>
2023-01-29 18:30:25 +0700

    up

--- src/main/java/com/ansv/taskmanagement/TaskManagementApplication.java
+++ src/main/java/com/ansv/taskmanagement/TaskManagementApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class TaskManagementApplication {
 
     public static void main(String[] args) {
