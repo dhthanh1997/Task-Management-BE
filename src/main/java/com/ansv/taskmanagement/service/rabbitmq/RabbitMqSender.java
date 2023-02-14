85987bfd7978ed4b44e975d44287c3f031b6fe89
BACHTUVU\PC <bachtuvu.dev@gmail.com>
2023-02-14 11:48:54 +0700

    update

--- src/main/java/com/ansv/taskmanagement/service/rabbitmq/RabbitMqSender.java
+++ src/main/java/com/ansv/taskmanagement/service/rabbitmq/RabbitMqSender.java
@@ -1,28 +1,28 @@
-package com.ansv.taskmanagement.service.rabbitmq;
-
-import com.ansv.taskmanagement.dto.response.UserDTO;
-import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
-
-@Service
-public class RabbitMqSender {
-
-    @Autowired
-    private AmqpTemplate rabbitTemplate;
-
-
-    @Value("${spring.rabbitmq.exchange-received:#{null}}")
-    private String exchange;
-
-    @Value("${spring.rabbitmq.routingkey-received:#{null}}")
-    private String routingkey;
-
-
-    public void sender(String username) {
-        rabbitTemplate.convertAndSend(exchange, routingkey, username);
-    }
-
-}
+//package com.ansv.taskmanagement.service.rabbitmq;
+//
+//import com.ansv.taskmanagement.dto.response.UserDTO;
+//import org.springframework.amqp.core.AmqpTemplate;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//
+//
+//@Service
+//public class RabbitMqSender {
+//
+//    @Autowired
+//    private AmqpTemplate rabbitTemplate;
+//
+//
+//    @Value("${spring.rabbitmq.exchange-received:#{null}}")
+//    private String exchange;
+//
+//    @Value("${spring.rabbitmq.routingkey-received:#{null}}")
+//    private String routingkey;
+//
+//
+//    public void sender(String username) {
+//        rabbitTemplate.convertAndSend(exchange, routingkey, username);
+//    }
+//
+//}
