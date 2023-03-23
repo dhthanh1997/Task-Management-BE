c6d37b267e49bb5d69f91377075c338b82cf211c
DESKTOP-QG1OPQ5\admin <huuthanh1308@gmail.com>
2023-03-23 14:16:45 +0700

    up

--- src/main/java/com/ansv/taskmanagement/repository/RedisRepository.java
+++ src/main/java/com/ansv/taskmanagement/repository/RedisRepository.java
@@ -1,60 +1,60 @@
-package com.ansv.taskmanagement.repository;
-
-import com.ansv.taskmanagement.dto.redis.AccessToken;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Repository;
-
-import java.util.Map;
-
-@Repository
-public class RedisRepository {
-
-    private static final Logger logger = LoggerFactory.getLogger(RedisRepository.class);
-
-    private HashOperations hashOperations;
-
-    private RedisTemplate redisTemplate;
-
-    private static final String ACCESSTOKEN = "accessToken";
-    private static final String REFRESHTOKEN = "refreshToken";
-
-    public RedisRepository(RedisTemplate redisTemplate) {
-        this.redisTemplate = redisTemplate;
-        this.hashOperations = this.redisTemplate.opsForHash();
-    }
-
-    public void saveToken(AccessToken token) {
-        try {
-            hashOperations.put(ACCESSTOKEN, token.getUuid(), token);
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-        }
-    };
-
-    public void updateToken(AccessToken token) {
-        saveToken(token);
-    };
-
-    public AccessToken getToken(String uuid) {
-        Map<String, Object> hashMaps = hashOperations.entries(ACCESSTOKEN);
-        Object token = hashOperations.get(ACCESSTOKEN,  uuid);
-        return (AccessToken) hashOperations.get(ACCESSTOKEN,  uuid);
-    };
-
-
-    public void getTokenToString(String uuid) {
-        Map<String, Object> hashMaps = hashOperations.entries(ACCESSTOKEN);
-//        Map<String, Object> mapsObject = hashOperations.get(ACCESSTOKEN,  uuid);
-        Object token = hashOperations.get(ACCESSTOKEN,  uuid);
-
-    };
-
-    public void deleteToken(String uuid) {
-        hashOperations.delete(ACCESSTOKEN, uuid);
-    };
-
-}
+//package com.ansv.taskmanagement.repository;
+//
+//import com.ansv.taskmanagement.dto.redis.AccessToken;
+//import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.data.redis.core.HashOperations;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Map;
+//
+//@Repository
+//public class RedisRepository {
+//
+//    private static final Logger logger = LoggerFactory.getLogger(RedisRepository.class);
+//
+//    private HashOperations hashOperations;
+//
+//    private RedisTemplate redisTemplate;
+//
+//    private static final String ACCESSTOKEN = "accessToken";
+//    private static final String REFRESHTOKEN = "refreshToken";
+//
+//    public RedisRepository(RedisTemplate redisTemplate) {
+//        this.redisTemplate = redisTemplate;
+//        this.hashOperations = this.redisTemplate.opsForHash();
+//    }
+//
+//    public void saveToken(AccessToken token) {
+//        try {
+//            hashOperations.put(ACCESSTOKEN, token.getUuid(), token);
+//        } catch (Exception e) {
+//            logger.error(e.getMessage(), e);
+//        }
+//    };
+//
+//    public void updateToken(AccessToken token) {
+//        saveToken(token);
+//    };
+//
+//    public AccessToken getToken(String uuid) {
+//        Map<String, Object> hashMaps = hashOperations.entries(ACCESSTOKEN);
+//        Object token = hashOperations.get(ACCESSTOKEN,  uuid);
+//        return (AccessToken) hashOperations.get(ACCESSTOKEN,  uuid);
+//    };
+//
+//
+//    public void getTokenToString(String uuid) {
+//        Map<String, Object> hashMaps = hashOperations.entries(ACCESSTOKEN);
+////        Map<String, Object> mapsObject = hashOperations.get(ACCESSTOKEN,  uuid);
+//        Object token = hashOperations.get(ACCESSTOKEN,  uuid);
+//
+//    };
+//
+//    public void deleteToken(String uuid) {
+//        hashOperations.delete(ACCESSTOKEN, uuid);
+//    };
+//
+//}
--- src/main/java/com/ansv/taskmanagement/repository/RedisTokenRepository.java
+++ src/main/java/com/ansv/taskmanagement/repository/RedisTokenRepository.java
@@ -1,33 +1,33 @@
-package com.ansv.taskmanagement.repository;
-
-
-import com.ansv.taskmanagement.dto.redis.AccessToken;
+//package com.ansv.taskmanagement.repository;
+//
+//
+//import com.ansv.taskmanagement.dto.redis.AccessToken;
+////import com.redis.om.spring.repository.RedisDocumentRepository;
 //import com.redis.om.spring.repository.RedisDocumentRepository;
-import com.redis.om.spring.repository.RedisDocumentRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-//@Repository
-public interface RedisTokenRepository extends RedisDocumentRepository<AccessToken, String> {
-//public interface RedisTokenRepository extends CrudRepository<AccessToken, String> {
-
-    Optional<AccessToken> findOneByUsername(String name);
-
-    Iterable<AccessToken> findByUsername(String name);
-
-    Iterable<AccessToken> findAllByUsername(String name);
-
-    Optional<AccessToken> findOneById(String uuid);
-
-    Optional<AccessToken> findById(String Id);
-
-    Iterable<AccessToken> findByUuid(String uuid);
-
-//    Iterable<AccessToken> findAll();
-
-
-}
-
-
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Optional;
+//
+////@Repository
+//public interface RedisTokenRepository extends RedisDocumentRepository<AccessToken, String> {
+////public interface RedisTokenRepository extends CrudRepository<AccessToken, String> {
+//
+//    Optional<AccessToken> findOneByUsername(String name);
+//
+//    Iterable<AccessToken> findByUsername(String name);
+//
+//    Iterable<AccessToken> findAllByUsername(String name);
+//
+//    Optional<AccessToken> findOneById(String uuid);
+//
+//    Optional<AccessToken> findById(String Id);
+//
+//    Iterable<AccessToken> findByUuid(String uuid);
+//
+////    Iterable<AccessToken> findAll();
+//
+//
+//}
+//
+//
