2794c1a663874df1d224862fdc70e3ca6720dbe6
DESKTOP-QG1OPQ5\admin <huuthanh1308@gmail.com>
2023-04-05 16:55:14 +0700

    up

--- src/main/java/com/ansv/taskmanagement/filter/JwtRequestFilter.java
+++ src/main/java/com/ansv/taskmanagement/filter/JwtRequestFilter.java
@@ -1,77 +1,77 @@
-package com.ansv.taskmanagement.filter;
-
-import com.ansv.taskmanagement.dto.response.UserDTO;
-import com.ansv.taskmanagement.handler.ApiError;
-import com.ansv.taskmanagement.handler.GlobalRestExceptionHandler;
-import com.ansv.taskmanagement.handler.authentication.JwtTokenNotValidException;
-//import com.ansv.taskmanagement.service.rabbitmq.RabbitMqReceiver;
-//import com.ansv.taskmanagement.service.rabbitmq.RabbitMqSender;
-import com.ansv.taskmanagement.util.DataUtils;
-import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.servlet.HandlerExceptionResolver;
-import org.springframework.web.util.ContentCachingResponseWrapper;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@Component
-@RequiredArgsConstructor
-@Slf4j
-public class JwtRequestFilter extends OncePerRequestFilter {
-
-    private final JwtTokenProvider jwtTokenProvider;
-    private static final Logger logger = LoggerFactory.getLogger(JwtRequestFilter.class);
-
-//    private RabbitMqSender rabbitMqSender;
-//    private RabbitMqReceiver rabbitMqReceiver;
-
-    @SneakyThrows
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException, AuthenticationException {
-        final String requestToken = request.getHeader("Authorization");
-        String requestURI = request.getRequestURI();
-        String jwtToken = null;
-        String username = null;
-        UserDTO userDTO = new UserDTO();
-//        if (DataUtils.notNullOrEmpty(requestToken)) {
-//            if (requestToken.startsWith("Bearer")) {
-//                jwtToken = requestToken.substring(7);
-//                username = jwtTokenProvider.getUsernameFromToken(jwtToken);
-//            } else {
-//                logger.warn("JWT token does not begin with Bearer string");
-//            }
+//package com.ansv.taskmanagement.filter;
 //
-//            if (DataUtils.notNullOrEmpty(username)) {
-////                // call from service in message bus
+//import com.ansv.taskmanagement.dto.response.UserDTO;
+//import com.ansv.taskmanagement.handler.ApiError;
+//import com.ansv.taskmanagement.handler.GlobalRestExceptionHandler;
+//import com.ansv.taskmanagement.handler.authentication.JwtTokenNotValidException;
+////import com.ansv.taskmanagement.service.rabbitmq.RabbitMqReceiver;
+////import com.ansv.taskmanagement.service.rabbitmq.RabbitMqSender;
+//import com.ansv.taskmanagement.util.DataUtils;
+//import lombok.RequiredArgsConstructor;
+//import lombok.SneakyThrows;
+//import lombok.extern.slf4j.Slf4j;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//import org.springframework.web.servlet.HandlerExceptionResolver;
+//import org.springframework.web.util.ContentCachingResponseWrapper;
+//
+//import javax.servlet.FilterChain;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//@Component
+//@RequiredArgsConstructor
+//@Slf4j
+//public class JwtRequestFilter extends OncePerRequestFilter {
+//
+//    private final JwtTokenProvider jwtTokenProvider;
+//    private static final Logger logger = LoggerFactory.getLogger(JwtRequestFilter.class);
+//
+////    private RabbitMqSender rabbitMqSender;
+////    private RabbitMqReceiver rabbitMqReceiver;
+//
+//    @SneakyThrows
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException, AuthenticationException {
+//        final String requestToken = request.getHeader("Authorization");
+//        String requestURI = request.getRequestURI();
+//        String jwtToken = null;
+//        String username = null;
+//        UserDTO userDTO = new UserDTO();
+////        if (DataUtils.notNullOrEmpty(requestToken)) {
+////            if (requestToken.startsWith("Bearer")) {
+////                jwtToken = requestToken.substring(7);
+////                username = jwtTokenProvider.getUsernameFromToken(jwtToken);
+////            } else {
+////                logger.warn("JWT token does not begin with Bearer string");
+////            }
+////
+////            if (DataUtils.notNullOrEmpty(username)) {
+//////                // call from service in message bus
+////
+////
+////            }
+////
+////        }
+////        else {
+////            throw new JwtTokenNotValidException("JWT token not valid");
+////        }
 //
 //
-//            }
+//        ContentCachingResponseWrapper responseCachingWrapper = new ContentCachingResponseWrapper((HttpServletResponse) response);
+//        filterChain.doFilter(request, response);
+//        // copy body to response
+//        responseCachingWrapper.copyBodyToResponse();
 //
-//        }
-//        else {
-//            throw new JwtTokenNotValidException("JWT token not valid");
-//        }
-
-
-        ContentCachingResponseWrapper responseCachingWrapper = new ContentCachingResponseWrapper((HttpServletResponse) response);
-        filterChain.doFilter(request, response);
-        // copy body to response
-        responseCachingWrapper.copyBodyToResponse();
-
-    }
-}
+//    }
+//}
--- src/main/java/com/ansv/taskmanagement/filter/JwtTokenProvider.java
+++ src/main/java/com/ansv/taskmanagement/filter/JwtTokenProvider.java
@@ -1,98 +1,98 @@
-package com.ansv.taskmanagement.filter;
-
-import com.ansv.taskmanagement.constants.JwtExceptionEnum;
-import io.jsonwebtoken.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.rest.webmvc.support.ExceptionMessage;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
-
-import java.util.Date;
-import java.util.List;
-
-@Component
-public class JwtTokenProvider {
-
-    private static final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
-
-    public String validateError = new String();
-
-    @Value("${app.jwtSecret}")
-    private String JWT_SECRET;
-
-    @Value("${app.tokenValidity}")
-    private String JWT_TOKEN_VALIDITY;
-
-
-    public Date getJwtTokenValidity() {
-        Date now = new Date();
-        return new Date(now.getTime() + JWT_TOKEN_VALIDITY);
-    }
-
-    public String getUUID(String token) {
-        Claims claims = Jwts.parser().setSigningKey((JWT_SECRET)).parseClaimsJws(token).getBody();
-//        Claims claims = Jwts.parser().parseClaimsJws(token).getBody();
-        return claims.get("uuid").toString();
-    }
-
-    public String getUsernameFromToken(String token) {
-        Claims claims = Jwts.parser().setSigningKey((JWT_SECRET)).parseClaimsJws(token).getBody();
-//        Claims claims = Jwts.parser().parseClaimsJws(token).getBody();
-        return claims.getSubject();
-    }
-
-    public String generateToken(Authentication authentication, String role, List<String> permissions) {
-        // using LDAP
-//        LdapUserDetailsImpl user = (LdapUserDetailsImpl)authentication.getPrincipal();
-        UserDetails user = (UserDetails) authentication.getPrincipal();
-        Date now = new Date();
-        Date expriryDate = new Date(now.getTime() + JWT_TOKEN_VALIDITY);
-        return Jwts.builder().setSubject(user.getUsername())
-                .setIssuedAt(now).setExpiration(expriryDate).signWith(SignatureAlgorithm.HS512, JWT_SECRET)
-//                .claim("role", role).claim("permissions", permissions)
-                .compact()
-                ;
-    }
-
-    public String generateToken(String username, String role, List<String> permissions) {
-        Date now = new Date();
-        Date expriryDate = new Date(now.getTime() + JWT_TOKEN_VALIDITY);
-        return Jwts.builder().setSubject(username).setIssuedAt(now).setExpiration(expriryDate)
-                .signWith(SignatureAlgorithm.HS512, JWT_SECRET)
-//                .claim("role", role).claim("permissions", permissions)
-                .compact();
-    }
-
-
-    public boolean validateToken(String token) {
-        try {
-            Jwts.parser().setSigningKey(JWT_SECRET).parseClaimsJws(token);
-            return true;
-        } catch (SignatureException e) {
-            logger.error("Invalid JWT signature", e);
-            this.validateError = JwtExceptionEnum.INVALID_JWT_SIGNATURE.getName();
-        } catch (MalformedJwtException e) {
-            logger.error("Invalid JWT Token ");
-            this.validateError = JwtExceptionEnum.INVALID_JWT_TOKEN.getName();
-
-        } catch (ExpiredJwtException e) {
-            logger.error("Expired JWT Token");
-            this.validateError = JwtExceptionEnum.EXPIRED_JWT_TOKEN.getName();
-
-        } catch (UnsupportedJwtException e) {
-            logger.error("Unsupported JWT Token");
-            this.validateError = JwtExceptionEnum.UNSUPPORT_JWT_TOKEN.getName();
-
-        } catch (IllegalArgumentException e) {
-            logger.error("JWT claims string is empty");
-            this.validateError = JwtExceptionEnum.JWT_CLAIMS_EMPTY.getName();
-
-        }
-        return false;
-    }
-
-
-}
+//package com.ansv.taskmanagement.filter;
+//
+//import com.ansv.taskmanagement.constants.JwtExceptionEnum;
+//import io.jsonwebtoken.*;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.data.rest.webmvc.support.ExceptionMessage;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.Date;
+//import java.util.List;
+//
+//@Component
+//public class JwtTokenProvider {
+//
+//    private static final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
+//
+//    public String validateError = new String();
+//
+//    @Value("${app.jwtSecret}")
+//    private String JWT_SECRET;
+//
+//    @Value("${app.tokenValidity}")
+//    private String JWT_TOKEN_VALIDITY;
+//
+//
+//    public Date getJwtTokenValidity() {
+//        Date now = new Date();
+//        return new Date(now.getTime() + JWT_TOKEN_VALIDITY);
+//    }
+//
+//    public String getUUID(String token) {
+//        Claims claims = Jwts.parser().setSigningKey((JWT_SECRET)).parseClaimsJws(token).getBody();
+////        Claims claims = Jwts.parser().parseClaimsJws(token).getBody();
+//        return claims.get("uuid").toString();
+//    }
+//
+//    public String getUsernameFromToken(String token) {
+//        Claims claims = Jwts.parser().setSigningKey((JWT_SECRET)).parseClaimsJws(token).getBody();
+////        Claims claims = Jwts.parser().parseClaimsJws(token).getBody();
+//        return claims.getSubject();
+//    }
+//
+//    public String generateToken(Authentication authentication, String role, List<String> permissions) {
+//        // using LDAP
+////        LdapUserDetailsImpl user = (LdapUserDetailsImpl)authentication.getPrincipal();
+//        UserDetails user = (UserDetails) authentication.getPrincipal();
+//        Date now = new Date();
+//        Date expriryDate = new Date(now.getTime() + JWT_TOKEN_VALIDITY);
+//        return Jwts.builder().setSubject(user.getUsername())
+//                .setIssuedAt(now).setExpiration(expriryDate).signWith(SignatureAlgorithm.HS512, JWT_SECRET)
+////                .claim("role", role).claim("permissions", permissions)
+//                .compact()
+//                ;
+//    }
+//
+//    public String generateToken(String username, String role, List<String> permissions) {
+//        Date now = new Date();
+//        Date expriryDate = new Date(now.getTime() + JWT_TOKEN_VALIDITY);
+//        return Jwts.builder().setSubject(username).setIssuedAt(now).setExpiration(expriryDate)
+//                .signWith(SignatureAlgorithm.HS512, JWT_SECRET)
+////                .claim("role", role).claim("permissions", permissions)
+//                .compact();
+//    }
+//
+//
+//    public boolean validateToken(String token) {
+//        try {
+//            Jwts.parser().setSigningKey(JWT_SECRET).parseClaimsJws(token);
+//            return true;
+//        } catch (SignatureException e) {
+//            logger.error("Invalid JWT signature", e);
+//            this.validateError = JwtExceptionEnum.INVALID_JWT_SIGNATURE.getName();
+//        } catch (MalformedJwtException e) {
+//            logger.error("Invalid JWT Token ");
+//            this.validateError = JwtExceptionEnum.INVALID_JWT_TOKEN.getName();
+//
+//        } catch (ExpiredJwtException e) {
+//            logger.error("Expired JWT Token");
+//            this.validateError = JwtExceptionEnum.EXPIRED_JWT_TOKEN.getName();
+//
+//        } catch (UnsupportedJwtException e) {
+//            logger.error("Unsupported JWT Token");
+//            this.validateError = JwtExceptionEnum.UNSUPPORT_JWT_TOKEN.getName();
+//
+//        } catch (IllegalArgumentException e) {
+//            logger.error("JWT claims string is empty");
+//            this.validateError = JwtExceptionEnum.JWT_CLAIMS_EMPTY.getName();
+//
+//        }
+//        return false;
+//    }
+//
+//
+//}
