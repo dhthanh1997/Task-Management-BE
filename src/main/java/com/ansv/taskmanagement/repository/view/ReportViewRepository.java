4e82ddc734357a21948364fff4e63d8a9c535477
DESKTOP-QG1OPQ5\admin <huuthanh1308@gmail.com>
2023-04-21 10:37:07 +0700

    up

--- src/main/java/com/ansv/taskmanagement/repository/view/ReportViewRepository.java
+++ src/main/java/com/ansv/taskmanagement/repository/view/ReportViewRepository.java
@@ -1,14 +1,14 @@
-package com.ansv.taskmanagement.repository.view;
-
-import com.ansv.taskmanagement.model.ReportView;
-import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
-
-@Component("ReportViewRepository")
-public interface ReportViewRepository extends ReadOnlyRepository<ReportView, Long> {
-
-    List<ReportView> findByUsernameOrTeamIdOrProjectId(Map<String, Object> parameters);
-}
+//package com.ansv.taskmanagement.repository.view;
+//
+//import com.ansv.taskmanagement.model.ReportView;
+//import org.springframework.stereotype.Component;
+//
+//import java.time.LocalDateTime;
+//import java.util.List;
+//import java.util.Map;
+//
+//@Component("ReportViewRepository")
+//public interface ReportViewRepository extends ReadOnlyRepository<ReportView, Long> {
+//
+//    List<ReportView> findByUsernameOrTeamIdOrProjectId(Map<String, Object> parameters);
+//}
